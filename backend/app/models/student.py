44424eafb1feb9fe9c643713ebdaf5c5f5c922ba
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-23 01:17:43 +0530

    refactor: Change id column type to UUID and update default generation method in Student model

--- backend/app/models/student.py
+++ backend/app/models/student.py
@@ -1,15 +1,20 @@
 import uuid
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 
 from app.db import Base
-def gen_uuid():
-    return str(uuid.uuid4())
+
 
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(Integer, primary_key=True, index=True, default=gen_uuid)
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        index=True
+    )
 
     reg_number = Column(
         String,
