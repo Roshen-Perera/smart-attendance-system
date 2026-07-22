7accbef1944d826becd08a6890dbef521ee817c1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-23 00:44:24 +0530

    feat: Include students router in main application file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.db import Base, engine
 from app.models import student
+from app.routers import students
 
 app = FastAPI(
     title="Smart Attendance API"
@@ -11,3 +12,7 @@ def root():
     return {
         "message": "Smart Attendance API running"
     }
+
+app.include_router(
+    students.router
+)
