bc8e2e985d2e852b462ab5e857fe5fe9354b8e25
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-23 00:13:33 +0530

    feat: Add database connection setup with SQLAlchemy

---
+++ backend/app/db.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://attendance_user:attendance_pass@localhost:5432/smart_attendance"
+)
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
