217aae88d922a5f6e7fa8aba0d185f90d349ceb5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-23 00:56:14 +0530

    feat: Implement student creation endpoint with validation for existing registration numbers

--- backend/app/routers/students.py
+++ backend/app/routers/students.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db import get_db
-from app.models.student import Student
+from app import models
+from app.schemas import student as schemas
 
 
 router = APIRouter(
@@ -11,9 +12,53 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def get_students(db: Session = Depends(get_db)):
+@router.post(
+    "",
+    response_model=schemas.StudentOut,
+    status_code=201
+)
+def create_student(
+    payload: schemas.StudentCreate,
+    db: Session = Depends(get_db)
+):
 
-    students = db.query(Student).all()
+    existing_student = (
+        db.query(models.Student)
+        .filter(
+            models.Student.reg_number == payload.reg_number
+        )
+        .first()
+    )
+
+    if existing_student:
+        raise HTTPException(
+            status_code=400,
+            detail="Student with this registration number already exists"
+        )
+
+
+    student = models.Student(
+        **payload.model_dump()
+    )
+
+    db.add(student)
+    db.commit()
+    db.refresh(student)
+
+    return student
+
+
+@router.get(
+    "",
+    response_model=list[schemas.StudentOut]
+)
+def get_students(
+    db: Session = Depends(get_db)
+):
+
+    students = (
+        db.query(models.Student)
+        .all()
+    )
 
     return students
