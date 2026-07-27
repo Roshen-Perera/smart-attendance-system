df9047caaf46a825bcbd44a6b6cac64836a5cc3c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-28 00:13:08 +0530

    updated with registering

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,1 +1,3 @@
-from .student import StudentCreate, StudentOut
+from .student import StudentCreate, StudentUpdate, StudentOut
+from .face import FaceImageOut
+from .lecturer import LecturerCreate, LecturerUpdate, LecturerOut
