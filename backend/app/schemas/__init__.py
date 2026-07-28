e40225e44569d80b8db74348cfa2bb9cdf28b6d4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-29 00:12:15 +0530

    initialized session schema

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .face import FaceImageOut
 from .lecturer import LecturerCreate, LecturerUpdate, LecturerOut
 from .class_course import ClassCreate, ClassUpdate, ClassOut
 from .enrollment import EnrollmentCreate, EnrollmentUpdate, EnrollmentOut
+from .session import SessionCreate, SessionUpdate, SessionOut
