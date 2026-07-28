97e2fdc60c6af699c7bdcdf7e4c1cdd80b75aae5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-28 15:07:07 +0530

    registered class schema

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .student import StudentCreate, StudentUpdate, StudentOut
 from .face import FaceImageOut
 from .lecturer import LecturerCreate, LecturerUpdate, LecturerOut
+from .class_course import ClassCreate, ClassUpdate, ClassOut
