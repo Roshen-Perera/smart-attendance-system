7f480a93668fb669d1f928e06dc9beff19d7506a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-29 00:35:44 +0530

    changed string to UUID

--- backend/app/models/session.py
+++ backend/app/models/session.py
@@ -18,7 +18,7 @@ class Session(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
     class_id = Column(
-        String,
+        UUID(as_uuid=True),
         ForeignKey("classes.id"),
         nullable=False
     )
