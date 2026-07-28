d76717117cfed2c681f5bcf729bc40fc7a0bbfc7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-07-28 23:27:34 +0530

    Registered the enrollment Schema

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .student import StudentCreate, StudentUpdate, StudentOut
 from .face import FaceImageOut
 from .lecturer import LecturerCreate, LecturerUpdate, LecturerOut
 from .class_course import ClassCreate, ClassUpdate, ClassOut
+from .enrollment import (EnrollmentCreate, EnrollmentUpdate, EnrollmentOut)
